8a9666c899e1753425bd3c1f48d0fbd4258a692d
user10oct25cl3 <seizedbharat@gmail.com>
2022-01-04 12:25:44 +0530

    simplify component

--- src/components/cards/ChartView.tsx
+++ src/components/cards/ChartView.tsx
@@ -3,18 +3,36 @@ import BarChart from '../BarChart'
 import { switchChart } from '../../redux/charts/chartActions'
 
 export default function ChartView() {
-    const selectedChart = useSelector<ChartState, ChartState["selectedChart"]>((state: any) => state.chart.selectedChart)
+    const chartType = useSelector<ChartState, ChartState["selectedChart"]>((state: any) => state.chart.selectedChart)
     const chartData = useSelector<ChartState, ChartState["chartData"]>((state: any) => state.chart.chartData)
     
     const dispatch = useDispatch<dispatchChartType>()
-    const clickHandler = (chartType: "alerts" |  "priority" | "overall") => dispatch(switchChart(chartType))
+    const clickHandler = (e: any) => dispatch(switchChart(e.target.id))
 
     return (
         <div className="cv-container">
             <div className="cv-btn-container">
-                <button onClick={evt => clickHandler('alerts')} className={`cv-btn ${selectedChart === "alerts" ? 'cv-btn-selected' : ''}`} title="Alerts">Alerts</button>
-                <button onClick={evt => clickHandler('priority')} className={`cv-btn ${selectedChart === "priority" ? 'cv-btn-selected' : ''}`} title="Priority">Priority</button>
-                <button onClick={evt => clickHandler('overall')} className={`cv-btn ${selectedChart === "overall" ? 'cv-btn-selected' : ''}`} title="Overall Readiness">Overall Readiness</button>
+                <button
+                    id='alerts'
+                    onClick={clickHandler}
+                    className={`cv-btn ${chartType === 'alerts' ? 'cv-selected' : ''}`}
+                >
+                    Alerts
+                </button>
+                <button
+                    id='priority'
+                    onClick={clickHandler}
+                    className={`cv-btn ${chartType === 'priority' ? 'cv-selected' : ''}`}
+                >
+                    Priority
+                </button>
+                <button
+                    id='overall'
+                    onClick={clickHandler}
+                    className={`cv-btn ${chartType === 'overall' ? 'cv-selected' : ''}`}
+                >
+                    Overall Readiness
+                </button>
             </div>
             <BarChart data={chartData} />
         </div>
